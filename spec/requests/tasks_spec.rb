f029021bc741cc53374745b1f59124527b63006e
Max Harris <mjharris@colorado.edu>
2015-01-26 14:44:22 -0700

    add 4 request rspec tests - to run: ~$ rspec spec/requests/

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
+
+
 RSpec.describe "Tasks", :type => :request do
-  # describe "GET /tasks" do
-  #   it "works! (now write some real specs)" do
-  #     get tasks_path
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+  describe "GET users/1/tasks" do
+    it "works! (now write some real specs)" do
+      get user_tasks_path(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET users/1/tasks/1" do
+  	it "works!" do
+  		test_user = User.create!(name: "Andy Dick", email: "test@email.com")
+  		test_task = test_user.tasks.create!(description: "Test Task", due_date: "1/30/15")
+  		get user_task_path(test_user,test_task)
+  		expect(response).to have_http_status(200)
+  	end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
   describe "GET /users" do
-    it "works! (now write some real specs)" do
+    it "retrieves all users" do
       get users_path
       expect(response).to have_http_status(200)
     end
+  end
+
+  describe "GET /user/:id" do
+  	it "retrieves a user from an id" do
+  		testUser = User.create!(name: "test", email:"test@email.com")
+  		get user_path(testUser)
+  		expect(response).to have_http_status(200)
+  	end
   end
 end
