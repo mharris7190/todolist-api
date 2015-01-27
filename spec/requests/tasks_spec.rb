76ea3dfa15f0df168ccd4033646b76860deecfa8
psudendra <psudendra@gmail.com>
2015-01-26 20:43:21 -0700

    updated rspec tests with put and delete

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,21 +1,51 @@
 require 'rails_helper'
 
-
-
-RSpec.describe "Tasks", :type => :request do
-  describe "GET users/1/tasks" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Tasks', :type => :request do
+  describe 'GET users/1/tasks' do
+    it 'works! (now write some real specs)' do
       get user_tasks_path(1)
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET users/1/tasks/1" do
-  	it "works!" do
-  		test_user = User.create!(name: "Andy Dick", email: "test@email.com")
-  		test_task = test_user.tasks.create!(description: "Test Task", due_date: "1/30/15")
+  describe 'GET users/1/tasks/1' do
+  	it 'works!' do
+  		test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
+  		test_task = test_user.tasks.create!(description: 'Test Task', due_date: '1/30/15')
   		get user_task_path(test_user,test_task)
   		expect(response).to have_http_status(200)
   	end
   end
+
+  describe 'POST /users/1/tasks/1#create' do
+    before do
+      test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
+    end
+
+    it 'add task to user' do
+      expect {
+        post '/users/1/tasks/1', :task => {description: 'Test Task', due_date: '1/30/15'}
+      }
+    end
+  end
+
+  describe 'PUT /users/1/tasks/:1#update' do
+    it 'update the user' do
+      test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
+      test_task = test_user.tasks.create!(description: 'Test Task', due_date: '1/30/15')
+      put '/users/1/tasks/1', :task =>  {:due_date => '2/05/15'}
+    end
+  end
+
+  describe 'DELETE destroy /users/1/tasks/1' do
+    before do
+      test_user = User.create!(name: 'test', email:'test@email.com')
+      test_task = test_user.tasks.create!(description: 'Test Task', due_date: '1/30/15')
+    end
+
+    it 'deletes the task' do
+      delete '/users/1/tasks/1'
+    end
+  end
+
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,26 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe "Users", :type => :request do
-  describe "GET /users" do
-    it "retrieves all users" do
+RSpec.describe 'Users', :type => :request do
+  describe 'GET /users' do
+    it 'retrieves all users' do
       get users_path
       expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /user/:id" do
-  	it "retrieves a user from an id" do
-  		testUser = User.create!(name: "test", email:"test@email.com")
+  describe 'GET /user/:id' do
+  	it 'retrieves a user from an id' do
+  		testUser = User.create!(name: 'test', email:'test@email.com')
   		get user_path(testUser)
   		expect(response).to have_http_status(200)
   	end
   end
 
-  describe "POST /users#create" do
+  describe 'POST create /users/1' do
     it 'should add one user' do
       expect {
-        post '/users.json', :user => {:name => 'Test', :login => 'test@test.com'}
+        post '/users.json', :user => {:name => 'Test', :email => 'test@test.com'}
       }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'PUT update /users/1' do
+    it 'update the user' do
+      @testUser = User.create!(name: 'test', email:'test@email.com')
+      put '/users/1', :user =>  {:email => 'newemail@email.com'}
+    end
+  end
+
+  describe 'DELETE destroy /users/1' do
+    before do
+      testUser = User.create!(name: 'test', email:'test@email.com')
+    end
+
+    it 'deletes the user' do
+      expect{
+        delete '/users/1'
+        }.to change(User, :count).by(-1)
     end
   end
 
