188759801fa1396bcf65a66ca5d5d1ea26323e70
Max Harris <mjharris@colorado.edu>
2015-01-26 20:24:35 -0700

    change 'login' to email, and check status code on post user to 201

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
+
+  before(:each) do 
+    @user1 = User.create!(name: "test", email:"test@email.com")
+  end
+
   describe "GET /users" do
     it "retrieves all users" do
       get users_path
@@ -10,8 +15,7 @@ RSpec.describe "Users", :type => :request do
 
   describe "GET /user/:id" do
   	it "retrieves a user from an id" do
-  		testUser = User.create!(name: "test", email:"test@email.com")
-  		get user_path(testUser)
+  		get user_path(@user1)
   		expect(response).to have_http_status(200)
   	end
   end
@@ -19,9 +23,14 @@ RSpec.describe "Users", :type => :request do
   describe "POST /users#create" do
     it 'should add one user' do
       expect {
-        post '/users.json', :user => {:name => 'Test', :login => 'test@test.com'}
+        post '/users.json', :user => {:name => 'Test', :email => 'test@test.com'}
       }.to change(User, :count).by(1)
     end
+
+    it 'shoud get 201 status code' do
+      post users_path(user: {name: "Test", email: "test@test.com"})
+      expect(response).to have_http_status(201)
+    end 
   end
 
 end
