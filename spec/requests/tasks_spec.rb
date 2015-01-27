f88617e8365de2b6c1808a8fec6ef8ca54147948
Max Harris <mjharris@colorado.edu>
2015-01-26 21:22:07 -0700

    add 2 explicit tests (one testing # of tasks) (one testing # of tasks associated with user1)

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -7,10 +7,10 @@ RSpec.describe 'Tasks', :type => :request do
     @task1 = @user1.tasks.create!(description: "task1", due_date: "2/1/15")
     @task2 = @user1.tasks.create!(description: "task2", due_date: "2/2/15")
   end
-  
+
   describe 'GET users/1/tasks' do
     it 'works! (now write some real specs)' do
-      get user_tasks_path(1)
+      get user_tasks_path(@task1)
       expect(response).to have_http_status(200)
     end
   end
@@ -23,14 +23,27 @@ RSpec.describe 'Tasks', :type => :request do
   end
 
   describe 'POST /users/:id/tasks/:id#create' do
-    before do
-      test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
+    # PRIYA's CODE: Wasn't doing anything
+    # before do
+    #   test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
+    # end
+
+    # it 'add task to user' do
+    #   expect {
+    #     post '/users/1/tasks/1', :task => {description: 'Test Task', due_date: '1/30/15'}
+    #   }
+    # end
+
+    before(:each) do
+      post user_tasks_path(@user1, task: {description: "task3", due_date: "2/3/15", user_id: @user1.id})
     end
 
-    it 'add task to user' do
-      expect {
-        post '/users/1/tasks/1', :task => {description: 'Test Task', due_date: '1/30/15'}
-      }
+    it "adds a task to the db" do
+      expect(Task.all.count).to eq(3)
+    end
+
+    it "associates the added task with the correct user" do
+      expect(@user1.tasks.count).to eq(3)
     end
   end
 
