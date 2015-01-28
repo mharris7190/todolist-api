169c8fabc864448f471348fb74ba5de0b8803945
Max Harris <mjharris@colorado.edu>
2015-01-27 21:46:58 -0700

    updates rspec tests to work through user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,25 @@
 class TasksController < ApplicationController
+  before_action :set_user
   before_action :set_task, only: [:show, :update, :destroy]
 
-  # GET /tasks
-  # GET /tasks.json
+  # GET users/1/tasks
+  # GET users/1/tasks.json
   def index
-    @tasks = Task.all
+    @tasks = @user.tasks
 
     render json: @tasks
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
+  # GET users/1/tasks/1
+  # GET users/1/tasks/1.json
   def show
     render json: @task
   end
 
-  # POST /tasks
-  # POST /tasks.json
+  # POST users/1/tasks
+  # POST users/1/tasks.json
   def create
-    @task = Task.new(task_params)
+    @task = @user.tasks.new(task_params)
 
     if @task.save
       render json: @task, status: :created, location: @task
@@ -27,20 +28,21 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
+  # PATCH/PUT users/1/tasks/1
+  # PATCH/PUT users/1/tasks/1.json
   def update
     @task = Task.find(params[:id])
 
     if @task.update(task_params)
-      head :no_content
+      render json: @task
+      # head :no_content
     else
       render json: @task.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
+  # DELETE users/1/tasks/1
+  # DELETE users/1/tasks/1.json
   def destroy
     @task.destroy
 
@@ -48,9 +50,12 @@ class TasksController < ApplicationController
   end
 
   private
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 
     def set_task
-      @task = Task.find(params[:id])
+      @task = @user.tasks.find(params[:id])
     end
 
     def task_params
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -17,25 +17,15 @@ RSpec.describe 'Tasks', :type => :request do
 
   describe 'GET users/:id/tasks/:id' do
   	it 'works!' do
-  		get user_task_path(@task1, @user1)
+  		get user_task_path(@user1, @task2)
   		expect(response).to have_http_status(200)
   	end
   end
 
+
   describe 'POST /users/:id/tasks/:id#create' do
-    # PRIYA's CODE: Wasn't doing anything
-    # before do
-    #   test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
-    # end
-
-    # it 'add task to user' do
-    #   expect {
-    #     post '/users/1/tasks/1', :task => {description: 'Test Task', due_date: '1/30/15'}
-    #   }
-    # end
-
     before(:each) do
-      post user_tasks_path(@user1, task: {description: "task3", due_date: "2/3/15", user_id: @user1.id})
+      post(user_tasks_path(@user1), task: {description: "task3", due_date: "2/3/15"})
     end
 
     it "adds a task to the db" do
@@ -47,40 +37,30 @@ RSpec.describe 'Tasks', :type => :request do
     end
   end
 
-# ALTERNATE WAY: USES FACTORY GIRL
-# def fake_user do
-#     @user1 = Factory.girl.create(:user)
-#     yield(@user1)
-#   end
-
-#     it 'adds a task to the db' do
-#       fake_user do |user|
-#         post :create, user_id: @user1.id, due_date: "2/3/15", description: "task3"
-#         expect(response).to eq 3
-#     end
-
-#     it "should associate task with the correct user" do
-#       expect(@user1.tasks.count).to eq 3
-#     end
 
 
   describe 'PUT /users/:id/tasks/:id#update' do
-    it 'update the user' do
-      test_user = User.create!(name: 'Andy Dick', email: 'test@email.com')
-      test_task = test_user.tasks.create!(description: 'Test Task', due_date: '1/30/15')
-      put '/users/1/tasks/1', :task =>  {:due_date => '2/05/15'}
+    before(:each) do
+      put("users/1/tasks/1", task: {description: "updated task1"})
+    end
+
+    it 'description should not still be the same' do
+      expect(JSON.parse(response.body)['description']).to_not eq("task1")
+    end
+
+    it 'updates the task description correctly' do
+      expect(JSON.parse(response.body)['description']).to eq "updated task1"
     end
   end
+
+
 
   describe 'DELETE destroy /users/:id/tasks/:id' do
-    before do
-      test_user = User.create!(name: 'test', email:'test@email.com')
-      test_task = test_user.tasks.create!(description: 'Test Task', due_date: '1/30/15')
-    end
 
-    it 'deletes the task' do
-      delete '/users/1/tasks/1'
+    it 'deletes task1 from user1' do
+      preCount = @user1.tasks.count
+      delete 'users/1/tasks/1'
+      expect(@user1.tasks.count).to eq(preCount-1)
     end
   end
-
 end
